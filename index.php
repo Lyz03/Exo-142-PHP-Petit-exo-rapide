630f710285f84fee10ca3bd4641cb6f589f5ee73
Lyz03 <lizoe@netcourrier.com>
2022-01-10 16:53:23 +0100

    start 142

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+    // part 1
+    echo "le texte de votre choix";
+
+    echo "<br>";
+    // part 2
+    $var = "Une chaîne de caractère";
+
+    echo $var;
+
+    echo "<br>";
+    // part 3
+    $array = ["entrée 1", "entrée 2", "entrée 3", "entrée 4"];
+
+    echo $array[1];
+
+    echo "<br>";
+    // part 4
+    for ($i = 0; $i <= 3; $i++) {
+        echo $array[$i] . "<br>";
+    }
+
+    // part 5
+    ?>
+
+    <ul>
+
+    </ul>
+
+</body>
+</html>
